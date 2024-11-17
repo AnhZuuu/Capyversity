01fcb9a61e5d123e53b7c18af8eb92b25ee41f21
Tien <thuytienln238@gmail.com>
2024-11-17 18:34:34 +0700

    fix &apos; in login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -82,7 +82,7 @@ const LoginPage: React.FC = () => {
         </button>{" "}
         <p className="mt-4 text-gray-400">
           {" "}
-          Don't have an account?{" "}
+          Don&apos;t have an account?{" "}
           <a href="/register" className="text-blue-400 hover:underline">
             Register
           </a>{" "}
