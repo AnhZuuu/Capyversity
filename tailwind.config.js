8dec9970d0cb63b5ea3eb511af6b8f4a8eff7383
Anh Vu <anhvu11102003@gmail.com>
2024-11-20 03:47:54 +0700

    add animation for tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,23 @@ module.exports = {
         lg: '0 8px 16px var(--tw-shadow-color)',
       },
     },
+    keyframes: {
+      wiggle: {
+        '0%, 100%': { transform: 'rotate(-15deg)' },
+        '50%': { transform: 'rotate(15deg)' },
+      },
+      heartBeat: {
+        '0%': { transform: 'scale(1);' },
+        '14%': { transform: 'scale(1.3);' },
+        '28%': { transform: 'scale(1);' },
+        '42%': { transform: 'scale(1.3);' },
+        '70%': { transform: 'scale(1);' },
+      },
+    },
+    animation: {
+      wiggle: 'wiggle 1s ease-in-out infinite',
+      heartBeat: 'heartBeat 1s infinite',
+    }
   },
   plugins: [nextui()],
 };
