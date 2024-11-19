be01f7eec769dc23951ef26c011bc56378a5e1ad
Tien <thuytienln238@gmail.com>
2024-11-20 06:09:56 +0700

    Done register function

---
+++ src/app/register/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import StyledComponentsRegistry from '@/lib/antd.registry';
+import RegisterPage from './page';
+import "@/app/css/App.css";
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Capyversity',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <StyledComponentsRegistry>
+          <RegisterPage/>
+        </StyledComponentsRegistry>
+      </body>
+    </html>
+  )
+}
