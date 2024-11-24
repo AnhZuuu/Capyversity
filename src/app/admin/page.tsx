ae18f507358680d9f4e5dabc195a77986724d2f5
Tien <thuytienln238@gmail.com>
2024-11-25 00:20:50 +0700

    Change access control + Add the logout button in admin page + Add the home button in Login page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useRouter } from "next/navigation";
 import React, { useEffect, useState, lazy, Suspense } from "react";
 
 const AdminUser = lazy(() => import("@/components/admin/user"));
@@ -7,46 +8,65 @@ const AdminNews = lazy(() => import("@/components/admin/news"));
 const AdminBlog = lazy(() => import("@/components/admin/blog"));
 
 const Admin = () => {
-    const [currentComponent, setCurrentComponent] = useState('user');
-    const handleShow = (component: any) => setCurrentComponent(component);
-    // const [isClient, setIsClient] = useState(false);
+  const [currentComponent, setCurrentComponent] = useState("user");
+  const handleShow = (component: any) => setCurrentComponent(component);
+  const router = useRouter();
+  const [userInfo, setUserInfo] = useState<any>(null);
 
-    // useEffect(() => {
-    //     setIsClient(true);
-    // }, []);
-    // // Only render the form after the component has mounted
-    // if (!isClient) {
-    //     return null; // Avoid rendering during SSR
-    // }
-    return (
-        <div className="flex h-screen">
-            {/* Left section for buttons */}
-            <div className="w-1/5 bg-gray-100 p-4">
-                {['user', 'school', 'news', 'blog'].map((item) => (
-                    <button
-                        key={item}
-                        className={`block w-full mb-2 p-2 rounded ${currentComponent === item
-                                ? 'bg-blue-700 text-white'
-                                : 'bg-blue-300 text-white hover:bg-blue-600'
-                            }`}
-                        onClick={() => handleShow(item)}
-                    >
-                        {item.charAt(0).toUpperCase() + item.slice(1)}
-                    </button>
-                ))}
-            </div>
+  const handleLogout = () => {
+    setUserInfo(null);
+    localStorage.removeItem("userInfo");
+    router.push("/login");
+  };
 
-            {/* Right section for components */}
-            <div className="w-4/5 p-4">
-                <Suspense fallback={<div>Loading...</div>}>
-                    {currentComponent === 'user' ? <AdminUser /> :
-                        currentComponent === 'school' ? <AdminSchool /> :
-                            currentComponent === 'news' ? <AdminNews /> :
-                                <AdminBlog />}
-                </Suspense>
-            </div>
-        </div>
-    );
+  useEffect(() => {
+    const storedUserInfo = localStorage.getItem("userInfo");
+    if (storedUserInfo) {
+      setUserInfo(JSON.parse(storedUserInfo));
+    }
+  }, []);
+
+  return (
+    <div className="flex h-screen">
+      {/* Left section for buttons */}
+      <div className="w-1/5 bg-gray-100 p-4">
+        {["user", "school", "news", "blog"].map((item) => (
+          <button
+            key={item}
+            className={`block w-full mb-2 p-2 rounded ${
+              currentComponent === item
+                ? "bg-blue-700 text-white"
+                : "bg-blue-300 text-white hover:bg-blue-600"
+            }`}
+            onClick={() => handleShow(item)}
+          >
+            {item.charAt(0).toUpperCase() + item.slice(1)}
+          </button>
+        ))}
+        <button          
+          className={`block w-full mb-2 p-2 rounded`}
+          onClick={handleLogout}
+        >
+          Logout
+        </button>
+      </div>
+
+      {/* Right section for components */}
+      <div className="w-4/5 p-4">
+        <Suspense fallback={<div>Loading...</div>}>
+          {currentComponent === "user" ? (
+            <AdminUser />
+          ) : currentComponent === "school" ? (
+            <AdminSchool />
+          ) : currentComponent === "news" ? (
+            <AdminNews />
+          ) : (
+            <AdminBlog />
+          )}
+        </Suspense>
+      </div>
+    </div>
+  );
 };
 
 export default Admin;
