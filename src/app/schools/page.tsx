9f42163e0cc254d1b71d92cc63df849c1ffffdc2
Anh Vu <anhvu11102003@gmail.com>
2024-11-24 13:52:08 +0700

    check userInfo in LocalStorage to show info EMAIL and ROLE only

--- src/app/schools/page.tsx
+++ src/app/schools/page.tsx
@@ -1,10 +1,18 @@
-import React from "react"
+'use client'
+import React, { useEffect, useState } from "react"
 import Schools from "@/components/schools/schools";
 import "@/app/css/App.css";
 import Schools_Premium1 from "@/components/schools_premium/schools_premium1";
 
-const SchoolsPage = async (props: any) => {
+const SchoolsPage = () => {
+    const [userInfo, setUserInfo] = useState<any>(null);
     // const [liked, setLiked] = useState(false);
+    useEffect(() => {
+        const storedUserInfo = localStorage.getItem("userInfo");
+        if (storedUserInfo) {
+          setUserInfo(JSON.parse(storedUserInfo));
+        }
+      }, []);
     return (
         <>
             <nav style={{ backgroundColor: '#383434' }}>
@@ -12,8 +20,9 @@ const SchoolsPage = async (props: any) => {
                     <span className='text-3xl text-white text-shadow-black text-md font-bold'>Trường</span>
                     <div className='text-1xl text-white text-shadow-black text-md'>Không chỉ là du học, mà là một cuộc phiêu lưu không ngừng...</div>
                 </div>
-                {/* <Schools /> */}
-                <Schools_Premium1/>
+                {userInfo ? <Schools_Premium1/> :<Schools /> }
+                {/*  */}
+                
             </nav>
         </>
     )
