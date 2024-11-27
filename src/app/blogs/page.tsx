b6a2a4a8468c4b93d75afc06d87f3ab9fc0cc0d8
Tien <thuytienln238@gmail.com>
2024-11-27 16:58:42 +0700

    Done normal blog page + Create blog_premium file + Fix image slide in News page +Delete old file don't use

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,16 +1,26 @@
-import React from 'react'
-import Blogs from '@/components/blogs/blogs'
+import React, { useEffect, useState } from 'react'
 import "@/app/css/App.css";
-
+import Blogs from '@/components/blogs/blogs';
+import BlogPremium from '@/components/blogs_premium/blog_premium';
 
 export default function BlogPage() {
+  const [userInfo, setUserInfo] = useState<any>(null);
+    
+    useEffect(() => {
+        const storedUserInfo = localStorage.getItem("userInfo");
+        if (storedUserInfo) {
+          setUserInfo(JSON.parse(storedUserInfo));
+        }
+      }, []);
+
   return (
     <nav style={{backgroundColor: '#383434'}}>
         <div style={{textAlign: 'center', paddingTop: '50px'}}>
             <span className='text-3xl text-white text-shadow-black text-md font-bold'>Trang chia sẻ</span>
             <div className='text-1xl text-white text-shadow-black text-md'>Nơi các bạn chia sẻ trải nghiệm và kinh nghiệm.</div>
         </div>
-        <Blogs/>
+        {/* <Blogs/> */}
+        {userInfo && userInfo[0].isPremium ? <BlogPremium/> :<Blogs/> }
     </nav> 
   )
 }
